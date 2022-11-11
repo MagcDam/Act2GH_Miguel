990843032ff8818b063060565cb4a6c804592540
MagcDam <a_miguelangel.gutierrez.calderon@iespablopicasso.es>
2022-11-11 13:52:30 +0100

    Version final del proyecto

--- ejercicio2.java
+++ ejercicio2.java
@@ -2,15 +2,23 @@ public class ejercicio2 {
 
   public static void main(String[] args) {
     final int NUMEROMAXIMO = 10;
-    final int CANTIDADNUMEROS = 20;
-    int[] numerosAleatorios = new int [20];
+    final int CANTIDADNUMEROS = 30;
+    int[] numerosAleatorios = new int [CANTIDADNUMEROS];
 
 
       for (int i = 0; i < CANTIDADNUMEROS; i++) {
         numerosAleatorios[i] = (int) (Math.random()*(NUMEROMAXIMO + 1));
 
-        System.out.println(numerosAleatorios[i]);
       }
+
+    int[] contadorNumeros = new int [CANTIDADNUMEROS];
+	for (int j = 0; j < CANTIDADNUMEROS; j++) {
+		for (int k = 0; k < CANTIDADNUMEROS; k++) {
+		}
+	Sistem.out.println([j] + 1])
+	}
+
+	}
 
 
 
